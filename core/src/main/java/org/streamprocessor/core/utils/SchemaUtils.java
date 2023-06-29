53668c41e2f175545343d1c59d5ab39aca33b1de
m0h318 <55669688+m0h318@users.noreply.github.com>
2023-06-29 10:24:59 +0300

    Bumped Beam SDK to 2.48.0

--- core/src/main/java/org/streamprocessor/core/utils/SchemaUtils.java
+++ core/src/main/java/org/streamprocessor/core/utils/SchemaUtils.java
@@ -24,15 +24,12 @@ import com.google.cloud.datacatalog.v1beta1.ColumnSchema;
 import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.beam.sdk.annotations.Experimental;
-import org.apache.beam.sdk.annotations.Experimental.Kind;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.Field;
 import org.apache.beam.sdk.schemas.Schema.FieldType;
 import org.apache.beam.sdk.schemas.logicaltypes.SqlTypes;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Strings;
 
-@Experimental(Kind.SCHEMAS)
 @SuppressWarnings({
     "rawtypes", // TODO(https://issues.apache.org/jira/browse/BEAM-10556)
     "nullness", // TODO(https://issues.apache.org/jira/browse/BEAM-10402)
