f20df25dfab0989bdbeda751682f88435c0e0c71
Mohamad El-baba <55669688+m0h318@users.noreply.github.com>
2023-08-30 15:38:53 +0200

    DATA-3285: Streamprocessor intermittent connection timeout from the data contracts service (#39)
    * added retries for data contract https request
    
    * logging
    
    * more logic for retry
    
    * improved retries
    
    * fixed formating
    
    * increased attempts

--- core/src/main/java/org/streamprocessor/core/utils/DataContractUtils.java
+++ core/src/main/java/org/streamprocessor/core/utils/DataContractUtils.java
@@ -3,6 +3,7 @@ package org.streamprocessor.core.utils;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.auth.http.HttpCredentialsAdapter;
@@ -11,8 +12,12 @@ import com.google.auth.oauth2.IdTokenCredentials;
 import com.google.auth.oauth2.IdTokenProvider;
 import java.io.IOException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataContractUtils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataContractUtils.class);
 
     private static HttpResponse makeGetRequest(String baseUrl, String endpointName)
             throws IOException {
@@ -41,14 +46,40 @@ public class DataContractUtils {
         return request.execute();
     }
 
-    public static JSONObject getDataContract(String endpoint) throws IOException {
+    public static JSONObject getDataContract(String endpoint)
+            throws IOException, InterruptedException, CustomExceptionsUtils.InvalidEntityException {
         String[] endpointComponents = endpoint.split("contract/");
         String baseUrl = endpointComponents[0];
         String topic = endpointComponents[1];
         String endpointName = "contract/" + topic;
 
-        HttpResponse response = makeGetRequest(baseUrl, endpointName);
-
-        return new JSONObject(response.parseAsString());
+        int attempt = 0;
+        int maxAttempts = 5;
+        String errorMessage = "";
+        while (maxAttempts > attempt) {
+            attempt++;
+            try {
+                HttpResponse response = makeGetRequest(baseUrl, endpointName);
+                return new JSONObject(response.parseAsString());
+            } catch (HttpResponseException e) {
+                errorMessage = e.getMessage();
+                if (e.getStatusCode() == 404 && errorMessage.contains("Topic not found")) {
+                    throw new CustomExceptionsUtils.InvalidEntityException(
+                            String.format("Entity [%s] don't exist.", topic));
+                }
+                Thread.sleep(2000);
+                LOG.warn(
+                        "Failed to get data contract: [{}]. Attempt [{}] of [{}]. Retrying..."
+                                + " error: [{}]",
+                        topic,
+                        attempt,
+                        maxAttempts,
+                        e.toString());
+            }
+        }
+        throw new IOException(
+                String.format(
+                        "Failed to get data contract [%s] after %d attempts. Error: [%s]",
+                        topic, attempt, errorMessage));
     }
 }
