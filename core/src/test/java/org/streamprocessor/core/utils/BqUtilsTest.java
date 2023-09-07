6dc22689b899737470b75ea63003c38fabcdee46
jbergenblom <jonathan.bergenblom@mathem.se>
2023-09-07 11:37:17 +0200

    Fix missing array element

--- core/src/test/java/org/streamprocessor/core/utils/BqUtilsTest.java
+++ core/src/test/java/org/streamprocessor/core/utils/BqUtilsTest.java
@@ -203,7 +203,7 @@ public class BqUtilsTest {
             Row.withSchema(FLAT_TYPE)
                     .addValues(
                             null, null, null, null, null, null, null, null, null, null, null, null,
-                            null, null, null, null, null, null, null, null, null, null, null, null)
+                            null, null, null, null, null, null, null, null, null, null, null, null, null)
                     .build();
 
     private static final TableRow BQ_NULL_FLAT_ROW =
